8fe7da98c9bdf114280fbc797f0a266754858961
Fardheen-N <104563302+Fardheen-N@users.noreply.github.com>
2024-01-03 10:44:35 +0530

    Create Best Time to Buy and Sell Stock.cpp

---
+++ Array/Best Time to Buy and Sell Stock.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int res=0;
+        int min=INT_MAX;
+        for(int i=0;i<prices.size();i++)
+        { 
+            if(prices[i]<min) min=prices[i];
+            else if(prices[i]-min>res){
+                res=prices[i]-min;
+            }
+        }
+        return res;
+    }
+};
