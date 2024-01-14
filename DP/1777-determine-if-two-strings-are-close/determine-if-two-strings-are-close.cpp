b51a7c6272b0efb9ae9a45378c343093c89e0f77
Fardheen-N <104563302+Fardheen-N@users.noreply.github.com>
2024-01-14 10:25:50 +0530

    Time: 58 ms (97.59%) | Memory: 17.4 MB (99.91%) - LeetSync

--- DP/1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
+++ DP/1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
@@ -1,25 +1,28 @@
 class Solution {
+        int Freq1[26]{};  
+        int Freq2[26]{};  
 public:
-    bool closeStrings(string word1, string word2) {
-        ios::sync_with_stdio(false);
-        cin.tie(0);
-        cout.tie(0);
-         map<char,int> mp;
-        vector<pair<int,char>> v1,v2;
-        for(auto i:word1) mp[i]++;
-        for(auto i:mp) v1.push_back({i.second,i.first});
-        mp.clear();
-        for(auto i:word2) mp[i]++;
-        for(auto i:mp) v2.push_back({i.second,i.first});
+    bool closeStrings(string &word1,string &word2){
+        int n = word1.size();
+        if(n != word2.size()) return false;
 
-        sort(v1.begin(),v1.end());
-        sort(v2.begin(),v2.end());
-
-        if(v1.size()!=v2.size()) return 0;
-        for(int i=0;i<v1.size();i++){
-            if(v1[i].first!=v2[i].first||mp[v1[i].second]==0) return 0;
+        for(int i=0;i<n;i++){
+            Freq1[word1[i]-'a']++;
+            Freq2[word2[i]-'a']++;
         }
-        return 1;
 
+        unordered_map<int,int> Hash;
+        
+        for(int i=0;i<26;i++){
+            if((Freq1[i]>0) ^ (Freq2[i]>0))return false;
+        }
+        sort(Freq1 ,Freq1+26);
+        sort(Freq2 ,Freq2+26);
+        
+        for(int i=0;i<26;i++){
+            if(Freq1[i]!=Freq2[i]) return false;
+        }
+
+        return true;
     }
 };
