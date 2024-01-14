920ac8dab2510bfb5963f916f8d6ceca2d4f64f0
Fardheen-N <104563302+Fardheen-N@users.noreply.github.com>
2024-01-14 10:24:22 +0530

    Time: 33 ms (99.74%) | Memory: 20.9 MB (78.40%) - LeetSync

--- DP/1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
+++ DP/1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     bool closeStrings(string word1, string word2) {
-        map<char,int> mp;
-        vector<pair<int,char>> v1,v2;
-        for(auto i:word1) mp[i]++;
-        for(auto i:mp) v1.push_back({i.second,i.first});
-        mp.clear();
-        for(auto i:word2) mp[i]++;
-        for(auto i:mp) v2.push_back({i.second,i.first});
+        ios::sync_with_stdio(false);
+        cin.tie(0);
+        cout.tie(0);
+        int n=word1.size();
+        int m=word2.size();
+        vector<int> freq1(26,0);
+        vector<int> freq2(26,0);
+        if(n!=m) return false;
+        for(int i=0;i<n;i++) freq1[word1[i]-97]++;
+        for(int i=0;i<m;i++) freq2[word2[i]-97]++;
+        for(int i=0;i<26;i++) if((freq1[i]==0 && freq2[i]!=0) || (freq1[i]==0 && freq2[i]!=0) ) return false;
+        sort(freq1.begin(),freq1.end());
+        sort(freq2.begin(),freq2.end());
+        return freq1==freq2;
 
-        sort(v1.begin(),v1.end());
-        sort(v2.begin(),v2.end());
-
-        if(v1.size()!=v2.size()) return 0;
-        for(int i=0;i<v1.size();i++){
-            if(v1[i].first!=v2[i].first||mp[v1[i].second]==0) return 0;
-        }
-        return 1;
     }
 };
