a54d7fe827682974e6edf02ed9a850b4d52220e8
Fardheen-N <104563302+Fardheen-N@users.noreply.github.com>
2024-03-26 10:19:11 +0530

    Time: 125 ms (5.51%) | Memory: 60.5 MB (15.15%) - LeetSync

--- Array/41-first-missing-positive/first-missing-positive.cpp
+++ Array/41-first-missing-positive/first-missing-positive.cpp
@@ -3,7 +3,7 @@ public:
     int firstMissingPositive(vector<int>& nums) {
         map<int,int> mp;
         for(auto i:nums) if(i>0) mp[i]++;
-        int res = -1;
+        // int res = -1;
         int pass = 0;
         for(auto i:mp){
             if(pass+1!=i.first) return pass+1;
