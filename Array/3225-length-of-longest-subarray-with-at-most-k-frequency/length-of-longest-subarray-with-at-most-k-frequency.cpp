04b1a91bb9b350c283e6ad352eba141a13b3443a
Fardheen-N <104563302+Fardheen-N@users.noreply.github.com>
2024-03-28 10:28:32 +0530

    Time: 163 ms (98.58%) | Memory: 148.1 MB (99.67%) - LeetSync

--- Array/3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ Array/3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     int maxSubarrayLength(vector<int>& nums, int k) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
         unordered_map<int,int> mp;
         int n = nums.size();
         int res = 0;
         int l=0;
-        int maxFreq = 0,ele =nums[0];
         for(int r=0;r<n;r++){
             mp[nums[r]]++;
-            if(mp[nums[r]]>maxFreq) {maxFreq = mp[nums[r]];ele = nums[r];}
-            while(mp[ele]>k && l<r){
-                mp[nums[l]]--;
-                l++;
+            while(mp[nums[r]]>k && l<r){
+                mp[nums[l++]]--;
             }
-            maxFreq = mp[ele];
             int len = r-l+1;
             res = max(len,res);
         }
