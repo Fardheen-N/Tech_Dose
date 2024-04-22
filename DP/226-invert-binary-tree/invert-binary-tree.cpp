b8e3cb0d3efc1e45ee1b0f5c455dd5bc3ad06637
Fardheen-N <104563302+Fardheen-N@users.noreply.github.com>
2024-04-22 09:59:42 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (51.59%) - LeetSync

--- DP/226-invert-binary-tree/invert-binary-tree.cpp
+++ DP/226-invert-binary-tree/invert-binary-tree.cpp
@@ -13,9 +13,10 @@ class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
         if(root==NULL) return NULL;
-        TreeNode *node = new TreeNode(root->val);
-        node->left = invertTree(root->right);
-        node->right = invertTree(root->left);
-        return node;
+
+        TreeNode *t = root->left;
+        root->left = invertTree(root->right);
+        root->right = invertTree(t);
+        return root;
     }
 };
