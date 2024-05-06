2a9cc0cefe77c96581f33a1f92a200602103bcf5
Fardheen-N <104563302+Fardheen-N@users.noreply.github.com>
2024-05-06 18:47:54 +0530

    Time: 43 ms (81.57%) | Memory: 48.1 MB (80.25%) - LeetSync

--- Tree/208-implement-trie-prefix-tree/implement-trie-prefix-tree.cpp
+++ Tree/208-implement-trie-prefix-tree/implement-trie-prefix-tree.cpp
@@ -1,42 +1,49 @@
-class TrieNode {
-public:
-    TrieNode* links[26];
-    bool isLeaf = false;
+class TrieNode{
+    public:
+        TrieNode *link[26];
+        bool leaf = 0;
 };
-
 class Trie {
-    TrieNode* root;
+    TrieNode *root;
 public:
     Trie() {
         root = new TrieNode();
     }
     
     void insert(string word) {
-        TrieNode* temp = root;
-        for(char c : word) {
-            if(!temp -> links[c - 'a']) {
-                temp -> links[c - 'a'] = new TrieNode();
+        TrieNode *temp = root;
+        for(auto i:word){
+            if(temp->link[i-'a']==NULL){
+                temp->link[i-'a']=new TrieNode();
             }
-            temp = temp -> links[c - 'a'];
+            temp=temp->link[i-'a'];
         }
-        temp -> isLeaf = true;
+        temp->leaf = true;
     }
-
+    
     bool search(string word) {
-        TrieNode* temp = root;
-        for(char c : word) {
-            if(!temp -> links[c - 'a']) return false;
-            temp = temp -> links[c - 'a'];
+        TrieNode *temp = root;
+        for(auto i:word){
+            if(!temp->link[i-'a']) return 0;
+            temp=temp->link[i-'a'];
         }
-        return temp -> isLeaf;
+        return temp->leaf;
     }
     
     bool startsWith(string prefix) {
-        TrieNode* temp = root;
-        for(char c : prefix) {
-            if(!temp -> links[c - 'a']) return false;
-            temp = temp -> links[c - 'a'];
+        TrieNode *temp = root;
+        for(auto i:prefix){
+            if(!temp->link[i-'a']) return 0;
+            temp=temp->link[i-'a'];
         }
         return true;
     }
 };
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
