92888358da8bc6e8c0f57bdd9360b96f0fed256a
Fardheen-N <104563302+Fardheen-N@users.noreply.github.com>
2024-07-08 09:34:56 +0530

    Time: 31 ms (9.75%) | Memory: 25.6 MB (20.59%) - LeetSync

---
+++ Tree/1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findTheWinner(int n, int k) {
+        queue<int> q;
+        for(int i=1;i<=n;i++) q.push(i); 
+        int cnt = 1;
+        while(q.size()!=1){
+            int ele = q.front();
+            q.pop();
+            if(cnt%k!=0){
+                q.push(ele);
+            }
+            cnt++;
+        }
+        return q.front();
+    }
+};
